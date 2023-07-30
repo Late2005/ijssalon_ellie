0b53caf9c9bac6c5ef37d0783c7e3bda20028c23
Marc van de Laar <marcvdlaar@hotmail.com>
2023-07-30 12:39:03 +0200

    Huiswerk les 10 toegevoegd

--- helper.py
+++ helper.py
@@ -5,3 +5,21 @@ def decoreer(tekst=""):
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def fooi_pp(bedrag, personen):
+    try:
+        bedrag_pp = bedrag / personen
+        return f"Het bedrag per persoon is {bedrag_pp:.f2} euro."
+    except:
+        return f"Voer alleen gehele getallen in groter dan nul (0)."
+
+#b = int(input("Welk bedrag zit er in de fooienpot?"))
+#p = int(input("Over hoeveel mensen moet de pot verdeeld worden?"))
+
+#print(fooi_pp(b,p))
+
+
+def som(dict):
+    totaal = list(dict.values())
+    return sum(totaal)
+
---
+++ presentatie.py
@@ -0,0 +1,9 @@
+def presenteer(dict,totaal):
+    totaal = totaal
+    for x, y in dict.items():
+        print(f"{x} : {y} euro")
+    print("==================================")
+    print(f"totaal : {totaal} euro")
+
+
+
